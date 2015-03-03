60f6363b1d6f2e10d100a68683276562c8751765
Brad <BradJohns94@gmail.com>
2015-03-02 19:34:37 -0500

    Added a sample util file

---
+++ util.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python2
+
+# util.py
+# This is the file which should containing the middleware functions to be accessed
+# by the frontend in order to access the database.
+
+def get_path(node1, node2):
+    """ Takes 2 node room #, building tuples, gets you a path between them """
+    return [ ('left', 'gumdrop lane'), ('right', 'This means nothing'), ('up', 'some stairs') ]
+
